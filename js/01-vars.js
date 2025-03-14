d48c1aa39deb57d88bf509c896c6c27b0708bdff
zhmen6ka25 <92273506+zhmen6ka25@users.noreply.github.com>
2025-03-14 18:47:02 +0100

    javascriptstart

---
+++ js/01-vars.js
@@ -0,0 +1,11 @@
+const apples = 47;
+const grapes = 135;
+console.log(apples)
+
+
+
+console.log(grapes)
+
+console.log(apples + grapes)
+
+console.log (grapes - apples)
---
+++ js/02-combined-operators.js
@@ -0,0 +1,3 @@
+let students = 100;
+students = students + 50;
+console.log(students);
